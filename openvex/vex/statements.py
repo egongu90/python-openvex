366e5bf7419a8c2d6d73772f3da9a36698fdf490
xNaaro <dabarren@gmail.com>
2025-05-02 16:50:00 +0200

    Initial draft commit for openvex

--- openvex/vex/statements.py
+++ openvex/vex/statements.py
@@ -1,12 +1,62 @@
-from dataclasses import dataclass
-import json
-
-# class Products:
-#     product = json.dumps({"@id": product}
-    # return json.dumps({"@id": product})
-
-@dataclass
 class Statements:
-    # product = ""
-    status = "under_investigation"
-    products = []
+    def return_status(self, status):
+        return status
+
+    def return_products(self, products):
+        return [
+            {
+                "@id": x.get("id", None),
+                "hashes": x.get("hashes", None),
+                "identifiers": x.get("identifiers", None),
+            }
+            for x in products
+        ]
+
+    def return_vulnerabilities(
+        self,
+        vulnerability_id,
+        vulnerability_name,
+        vulnerability_description,
+        vulnerability_aliases,
+    ):
+        return {
+            "@id": vulnerability_id,
+            "name": vulnerability_name,
+            "description": vulnerability_description,
+            "aliases": vulnerability_aliases,
+        }
+
+    def return_justification(self, justification):
+        return justification
+
+    def return_impact_statement(self, impact_statement):
+        return impact_statement
+
+    # @staticmethod
+    def return_statements(
+        self,
+        status,
+        products,
+        vulnerability_id,
+        vulnerability_name,
+        vulnerability_description,
+        vulnerability_aliases,
+        justification,
+        impact_statement,
+    ):
+        return [
+            {
+                "vulnerability": self.return_vulnerabilities(
+                    vulnerability_id,
+                    vulnerability_name,
+                    vulnerability_description,
+                    vulnerability_aliases,
+                ),
+                "products": self.return_products(products),
+                "status": self.return_status(status),
+                "justification": self.return_justification(justification),
+                "impact_statement": self.return_impact_statement(
+                    impact_statement
+                ),
+            }
+        ]
--- openvex/vex/vex.py
+++ openvex/vex/vex.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+import json
 from dataclasses import dataclass
+from datetime import datetime
+
 from openvex.vex.statements import Statements
 
-import json
 
 @dataclass
 class Vex:
@@ -15,16 +16,32 @@ class Vex:
     spec_version = "0.2.0"
     public_namespace = "https://openvex.dev/docs"
     timestamp = str(datetime.now())
-    statements = [Statements()]
+    products = []
+    status = ""
+    vulnerability_id = ""
+    vulnerability_name = ""
+    vulnerability_description = ""
+    vulnerability_aliases = []
+    justification = ""
+    impact_statement = ""
 
     def new(self):
-        document = {"@context": self.context,
-                    "@id": self.id,
-                    "author": self.author,
-                    "role": self.role,
-                    "timestamp": self.timestamp,
-                    "version": self.version,
-                    "statements": self.statements,
-                   }
+        document = {
+            "@context": self.context,
+            "@id": self.id,
+            "author": self.author,
+            "role": self.role,
+            "timestamp": self.timestamp,
+            "version": self.version,
+            "statements": Statements().return_statements(
+                self.status,
+                self.products,
+                self.vulnerability_id,
+                self.vulnerability_name,
+                self.vulnerability_description,
+                self.vulnerability_aliases,
+                self.justification,
+                self.impact_statement,
+            ),
+        }
         return json.dumps(document, default=vars)
-
